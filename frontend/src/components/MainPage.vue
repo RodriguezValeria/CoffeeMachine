0b49ac3852d3598dbbd17b37578cd85271f6f99b
RodriguezValeria <valeria.rodriguez21@ucr.ac.cr>
2024-12-05 08:35:27 -0600

    Changes added.

--- frontend/src/components/MainPage.vue
+++ frontend/src/components/MainPage.vue
@@ -1,12 +1,18 @@
 <template>
-  <div class="main-page">
+  <div :class="{ 'blur-background': isAddModalVisible }" class="main-page">
     <div class="main-page-header">
       <h1 class="header-title">Vale's Coffee Nest</h1>
+      <div class="header-info">
+        <div class="date"> {{ new Date().toLocaleDateString() }} </div>
+        <div class="total-price">
+          Total: ₡{{ priceTotal }}
+        </div>
+      </div>
     </div>
     <div class="coffee-main-section">
       Seleccione su café de preferencia:
       <div class="coffee-list">
-        <div class="coffee-container" v-for="coffee in coffeeList" :key="coffee.name">
+        <div class="coffee-container" v-for="coffee in coffeeList" :key="coffee.name" @click="selectCoffee(coffee)">
           <div class="coffee-image">
             <img :src="getImage(coffee.image)" alt="Coffee" width="200" height="200">
           </div>
@@ -19,18 +25,57 @@
       </div>
     </div>
   </div>
+
+  <ModalComponent
+    :isVisible="isAddModalVisible"
+    title="Agregar a la orden"
+    @close="isModalVisible = false"
+  >
+    <template #body>
+      <div v-if="selectedCoffee" class="modal-content">
+        <h2>{{ selectedCoffee.coffeeType }}</h2>
+        <img :src="getImage(selectedCoffee.image)" alt="Coffee Image" width="200" height="200">
+        <p class="coffee-price-modal">₡{{ selectedCoffee.price * quantity}}</p>
+        Ingrese la cantidad deseada:
+        <div class="input-group-modal">
+          <svg @click="decreaseQuantity" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-dash-circle" viewBox="0 0 16 16">
+            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+            <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8"/>
+          </svg>
+          <input type="number" min="1" :max="selectedCoffee.quantity" v-model="quantity" placeholder="1" class="input-field"/>
+          <svg @click="increaseQuantity" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+          </svg>
+      </div>
+      </div>
+    </template>
+    <template v-slot:footer>
+      <button @click="isModalVisible = false" type="button" class="custom-button custom-font white-font">Agregar</button>
+      <button @click="isAddModalVisible = false" type="button" class="custom-button custom-font white-font">Cerrar</button>
+    </template>
+  </ModalComponent>
+
 </template>
 
 <script>
 import axios from 'axios'
 import { BACKEND_URL } from "@/config";
+import ModalComponent from "./ModalComponent.vue";
 
 
 export default {
   name: 'MainPage',
+  components: {
+    ModalComponent
+  },
   data() {
     return {
-      coffeeList: []
+      coffeeList: [],
+      isAddModalVisible: false,
+      selectedCoffee: null,
+      quantity: 1,
+      priceTotal: 0,
     }
   },
   methods: {
@@ -38,7 +83,6 @@ export default {
       try {
         const response = await axios.get(`${BACKEND_URL}/Coffee`);
         this.coffeeList = response.data;
-        console.log(this.coffeeList);
       } catch (error) {
         console.error(error)
       }
@@ -46,6 +90,25 @@ export default {
     getImage(imagePath) {
       const url = `https://localhost:7263${imagePath}`;
       return url;
+    },
+    showModal() {
+      this.isAddModalVisible = true;
+      console.log("Modal triggered");
+    },
+    selectCoffee(coffee) {
+      this.selectedCoffee = coffee;
+      this.quantity = 1;
+      this.showModal();
+    },
+    increaseQuantity() {
+      if (this.quantity < this.selectedCoffee.quantity) {
+        this.quantity++;
+      }
+    },
+    decreaseQuantity() {
+      if (this.quantity > 0) {
+        this.quantity--;
+      }
     }
   },
   mounted() {
@@ -57,10 +120,19 @@ export default {
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Mooli&display=swap');
 
+.blur-background {
+  filter: blur(5px);
+  opacity: 0.7;
+}
+
 .main-page-header {
+  display: flex;
   background-color: #310603;
-  padding: 40px;
+  justify-content: space-between;
+  align-items: center;
+  padding: 32px;
   margin: 0;
+  position: relative;
 }
 
 .header-title {
@@ -69,7 +141,33 @@ export default {
   font-family: "Mooli", sans-serif;
   font-weight: 100;
   font-size: 2.5em;
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
   margin: 0;
+}
+
+.header-info {
+  margin-left: auto;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+
+.date {
+  color: #ffffff;
+  font-family: "Montserrat", sans-serif;
+  font-weight: 300;
+  font-size: 25px;
+  margin: 0;
+}
+
+.total-price {
+  color: #ffffff;
+  font-family: "Montserrat", sans-serif;
+  font-weight: 300;
+  font-size: 1.5em;
+  margin-left: 20px;
 }
 
 .coffee-main-section {
@@ -98,6 +196,11 @@ export default {
   align-items: center;
 }
 
+  .coffee-container:hover {
+    background-color: #815341;
+    cursor: pointer;
+  }
+
 .coffee-info {
   margin-left: 30px;
 }
@@ -119,6 +222,40 @@ export default {
   font-weight: 400;
   text-align: center;
   margin-top: 20px;
+}
+
+.modal-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px;
+}
+
+.coffee-price-modal {
+  font-size: 30px;
+  font-weight: 500;
+  margin-top: 20px;
+}
+
+.input-group-modal {
+  display: flex;
+  align-items: center;
+}
+
+.input-field {
+  margin-left: 8px; 
+  margin-right: 8px;
+}
+
+/* Hide spinner controls in modern browsers */
+input[type="number"]::-webkit-inner-spin-button,
+input[type="number"]::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+input[type="number"] {
+  -moz-appearance: textfield; /* Hide spinner for Firefox */
 }
 
 </style>
---
+++ frontend/src/components/ModalComponent.vue
@@ -0,0 +1,106 @@
+<template>
+  <div v-if="isVisible">
+    <div class="modal-backdrop"></div>
+    <div
+      class="modal fade show d-block"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalComponentsLabel"
+    >
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <!-- Modal Header -->
+          <div class="modal-header custom-header">
+            <h5 class="modal-title custom-font white-font" id="exampleModalComponentsLabel">
+              {{ title }}
+            </h5>
+          </div>
+          <!-- Modal Body -->
+          <div class="modal-body custom-font black-font">
+            <slot name="body"></slot>
+          </div>
+          <!-- Modal Footer -->
+          <div class="modal-footer">
+            <slot name="footer">
+              <button @click="closeModal" type="button" class="custom-button custom-font white-font">Cerrar</button>
+            </slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "ModalComponent",
+  props: {
+    isVisible: {
+      type: Boolean,
+      required: true,
+    },
+    title: {
+      type: String,
+      default: "Modal title",
+    },
+  },
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style>
+
+.modal {
+  display: block;
+}
+
+.custom-font {
+  font-size: 20px;
+  font-feature-settings: 'liga' off, 'clig' off;
+  font-family: Montserrat;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 500;
+}
+
+.black-font {
+  color: #000;
+}
+
+.white-font {
+  color: var(--background, #fbfbfe);
+}
+
+.custom-header {
+  background: var(--buttons-primary, #310603);
+  max-width: 100%;
+  padding: 10px 20px;
+  align-content: center;
+  border: none;
+}
+
+.custom-button {
+  border-radius: 5px;
+  background: var(--buttons-primary, #310603);
+  max-width: 100%;
+  padding: 10px 20px;
+  align-content: center;
+  border: none;
+}
+
+.modal-backdrop {
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 1050;
+}
+
+</style>
